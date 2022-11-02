0239630168873411d9846a4d33aa08cfc78d8be7
Olachi Efika <olachi_efika@yahoo.com>
2022-11-01 20:37:30 -0400

    Updated visitor counter

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,20 @@
-function websiteVisits(response) {
-    document.querySelector("#visits").textContent = response.value;
+<body onload="updateCounter()">
+    <script>
+function updateCounter(){
+    fetch('https://vamzxh0jag.execute-api.ca-central-1.amazonaws.com/prod',{
+        method: 'GET'
+    })
+  .then(response => {
+    if (
+        // check if response's status is 200
+        response.ok
+    ) {
+      return response.json()
+    } else {
+      throw new Error('something went wrong');
+    }
+  })
+  .then(data => document.getElementById("hits").innerText = data.Visit_Count)
 }
+</script>
+    Visits: <span id="hits"></span>
